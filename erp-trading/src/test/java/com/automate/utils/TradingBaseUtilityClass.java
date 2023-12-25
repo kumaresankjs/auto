da7b4f8eb8f417643ad9ab5fcbc6112de83d34f5
kumaresan <kumaresankjs@gmail.com>
2023-12-25 13:41:36 +0530

    common changes made in tradingbase utility class in trunk1.0

--- erp-trading/src/test/java/com/automate/utils/TradingBaseUtilityClass.java
+++ erp-trading/src/test/java/com/automate/utils/TradingBaseUtilityClass.java
@@ -16,7 +16,6 @@ import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
@@ -59,7 +58,6 @@ public class TradingBaseUtilityClass {
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(property.getProperty("demoUrl"));
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("form:login-focus:userId")));
 	}
 	
 	@AfterMethod(alwaysRun = true)
